8b269bbe7d4e5ed74e4bf678f47e6ca2ee949b1b
Robbie Pope <robbiepope93@gmail.com>
2023-10-27 15:28:35 +0100

    20231027

--- R/estimate_bonferroni.R
+++ R/estimate_bonferroni.R
@@ -1,18 +1,17 @@
 ################################################################################
-# Estimate bonferroni correction 
+# Estimate bonferroni correction
 ################################################################################
-
 # Function to estimatr the bonferroni correction
-estimate.bonferroni <- function(meff, signific=0.05) 
+estimate.bonferroni <- function(meff, signific=0.05)
   #' Estimates the bonferroni corrected p-value threshold
-  #' 
+  #'
   #' @param meff <- maximum effective number of tests (float)
   #' @param signific <- Researcher set sginificance value, default is 0.05
   #' @return bonferroni corrected p value
-  #' 
+  #'
   #' @author Robbie Pope
-  #' 
-  
+  #'
+
 {
   m.eff <- 1/meff
   alpha <- 1 - (1-signific)**m.eff
